31fb38bad5007e7f8518cb17c78c03d23591bde4
luovabr <leite@luova.dev.br>
2022-08-23 23:37:17 -0300

    23.08.2022

--- Project1/tailwind.config.js
+++ Project1/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
-      
+      'comf': ['Comfortaa', 'sans-serif'],
+      'lora': ['Lora', 'serif'],
+      'nun': ['Nunito', 'sans-serif'],
+      'libre': ['Libre Baskerville', 'serif'], 
+      'monte': ['Montserrat', 'sans-serif'],
+      'sans': ['Nunito', ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 npm install -D tailwindcss
 npx tailwindcss init
+npx tailwindcss -i ./src/css/input.css -o ./dist/css/utput.css --watch
 
 # Figma
 
